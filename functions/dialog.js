fc8ee1e846765e17fa6efeaff873d48c5ce10b06
Jacob <jacoblee93@gmail.com>
2018-05-11 11:33:31 -0700

    Improves comments in dialog.js

--- functions/dialog.js
+++ functions/dialog.js
@@ -4,21 +4,7 @@ const getBotToken = require('../helpers/get_bot_token.js');
 const message = require('../utils/message.js');
 
 /**
- * Slack Actions (Interactive Messages) Response Handler
- *   This function receives actions (interactive messages) from Slack and
- *     dispatches the appropriate handler. You should use this function as the
- *     endpoint for all actions, and place action handlers in
- *     /functions/actions/NAME.js, where NAME is the name parameter of the
- *     action you are dispatching from your interactive message.
- *
- *   You can test from the command line using:
- *     lib .actions.NAME [username] [channel name]
- *
- *   For more about interactive messages and how to respond to them, see Slack's
- *     documentation: https://api.slack.com/docs/message-buttons
- *
- *   You should not need to modify this file to get a basic Slack app running.
- *
+ * Slack Dialog (Interactive Messages) Response Handler
  * @bg empty
  * @returns {object}
  */
@@ -39,7 +25,7 @@ module.exports = (context, callback) => {
     return callback(null, {error: 'No dialog specified'});
   }
 
-  // Use dialog.callback_id to distinguish between different dialogs
+  // Use dialog.callback_id to distinguish between different dialogs if you need to
 
   let user = dialog.user; // Object with username and id
   let channel = dialog.channel; // Object with name and id
